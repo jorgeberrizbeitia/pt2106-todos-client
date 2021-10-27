1ad5023b5f152dee392ea1775c19c74e860761fa
Alntjan <oliveiracatarino88@gmail.com>
2021-10-27 09:24:14 +0100

    M3W5D1

--- src/components/TodoDetails.jsx
+++ src/components/TodoDetails.jsx
@@ -1,57 +1,61 @@
-import axios from 'axios'
-import React, { Component } from 'react'
+import axios from 'axios';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class TodoDetails extends Component {
-
   state = {
     singleTodo: null,
-    isLoading: true
-  }
+    isLoading: true,
+  };
 
   componentDidMount() {
-    axios.get(`${process.env.REACT_APP_SERVER_API}/todos/${this.props.match.params.id}`)
-    .then( (response) => this.setState({ singleTodo:response.data, isLoading: false }))
-    .catch( () => this.props.history.push("/500"));
+    axios
+      .get(
+        `${process.env.REACT_APP_SERVER_API}/todos/${this.props.match.params.id}`
+      )
+      .then((response) =>
+        this.setState({ singleTodo: response.data, isLoading: false })
+      )
+      .catch(() => this.props.history.push('/500'));
   }
 
   handleDelete = () => {
-    axios.delete(`${process.env.REACT_APP_SERVER_API}/todos/${this.props.match.params.id}`)
-    .then( () => this.props.history.push("/"))
-    .catch( () => this.props.history.push("/500"));
-  }
+    axios
+      .delete(
+        `${process.env.REACT_APP_SERVER_API}/todos/${this.props.match.params.id}`
+      )
+      .then(() => this.props.history.push('/'))
+      .catch(() => this.props.history.push('/500'));
+  };
 
   render() {
-
-    const { isLoading, singleTodo } = this.state
+    const { isLoading, singleTodo } = this.state;
 
     return (
       <div>
-
         <h2>Todo Details</h2>
 
-        { isLoading && <h1>...Loading</h1>}
+        {isLoading && <h1>...Loading</h1>}
 
-        { !isLoading && (
+        {!isLoading && (
           <div>
             <h4>Title: {singleTodo.title} </h4>
             <p>Description: {singleTodo.description}</p>
             {/* <p>Do Before: {singleTodo.doBefore.slice(0, 10)} </p> */}
             <p>Do Before: {new Date(singleTodo.doBefore).toDateString()} </p>
-
+            {singleTodo.imageUrl && (
+              <img src={singleTodo.imageUrl} alt="todo" />
+            )}
             <button onClick={this.handleDelete}>DELETE</button>
 
             <Link to={`/todo/${singleTodo._id}/edit`}>
               <button>EDIT</button>
             </Link>
-            
-
           </div>
-        ) }
-
+        )}
       </div>
-    )
+    );
   }
 }
 
-export default TodoDetails
+export default TodoDetails;
